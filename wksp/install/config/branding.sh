3b2cc580750be4e4c5fc59515c433f825644619c
criticalart <heels.rower.7z@icloud.com>
2025-09-15 01:08:31 -0700

    Delete wksp directory

--- wksp/install/config/branding.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Allow the user to change the branding for fastfetch and screensaver
-mkdir -p ~/.config/calos/branding
-cp ~/.local/share/calos/icon.txt ~/.config/calos/branding/about.txt
-cp ~/.local/share/calos/logo.txt ~/.config/calos/branding/screensaver.txt
--- wksp/install/config/config.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Copy over Omarchy configs
-mkdir -p ~/.config
-cp -R ~/.local/share/calos/config/* ~/.config/
-
-# Use default bashrc from Omarchy
-cp ~/.local/share/calos/default/bashrc ~/.bashrc
