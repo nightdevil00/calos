e1c582fc845278aa24d908f33d137a1e93e2c6df
criticalart <heels.rower.7z@icloud.com>
2025-09-29 19:43:46 -0700

    final changes prior to walker fix

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@
 set -eE
 
 export PATH="$HOME/.local/share/calos/bin:$PATH"
-OMARCHY_INSTALL=~/.local/share/calos/install
+CALOS_INSTALL=~/.local/share/calos/install
 
 clear
 echo "Installation Starting..."
@@ -26,40 +26,38 @@ echo "ExecStart=-/sbin/agetty -o '-p -- $USER' --noclear --skip-login - "'$TERM'
 sleep 1
 
 # Preparation
-source $OMARCHY_INSTALL/preflight/show-env.sh
-source $OMARCHY_INSTALL/preflight/trap-errors.sh
-# source $OMARCHY_INSTALL/preflight/guard.sh
-source $OMARCHY_INSTALL/preflight/chroot.sh
-source $OMARCHY_INSTALL/preflight/repositories.sh
+source $CALOS_INSTALL/preflight/show-env.sh
+source $CALOS_INSTALL/preflight/trap-errors.sh
+source $CALOS_INSTALL/preflight/chroot.sh
+source $CALOS_INSTALL/preflight/repositories.sh
 
 # Packaging
-source $OMARCHY_INSTALL/packages.sh
-source $OMARCHY_INSTALL/packaging/fonts.sh
-source $OMARCHY_INSTALL/packaging/lazyvim.sh
-source $OMARCHY_INSTALL/packaging/tuis.sh
+source $CALOS_INSTALL/packages.sh
+source $CALOS_INSTALL/packaging/fonts.sh
+source $CALOS_INSTALL/packaging/lazyvim.sh
+source $CALOS_INSTALL/packaging/tuis.sh
 
 # Configuration
-source $OMARCHY_INSTALL/config/config.sh
-source $OMARCHY_INSTALL/config/theme.sh
-source $OMARCHY_INSTALL/config/branding.sh
-source $OMARCHY_INSTALL/config/git.sh
-source $OMARCHY_INSTALL/config/gpg.sh
-source $OMARCHY_INSTALL/config/timezones.sh
-source $OMARCHY_INSTALL/config/increase-sudo-tries.sh
-source $OMARCHY_INSTALL/config/increase-lockout-limit.sh
-source $OMARCHY_INSTALL/config/ssh-flakiness.sh
-source $OMARCHY_INSTALL/config/detect-keyboard-layout.sh
-# source $OMARCHY_INSTALL/config/mimetypes.sh
-source $OMARCHY_INSTALL/config/localdb.sh
-source $OMARCHY_INSTALL/config/hardware/network.sh
-source $OMARCHY_INSTALL/config/hardware/fix-fkeys.sh
-source $OMARCHY_INSTALL/config/hardware/bluetooth.sh
-source $OMARCHY_INSTALL/config/hardware/usb-autosuspend.sh
-source $OMARCHY_INSTALL/config/hardware/ignore-power-button.sh
-source $OMARCHY_INSTALL/config/hardware/nvidia.sh
+source $CALOS_INSTALL/config/config.sh
+source $CALOS_INSTALL/config/theme.sh
+source $CALOS_INSTALL/config/branding.sh
+source $CALOS_INSTALL/config/git.sh
+source $CALOS_INSTALL/config/gpg.sh
+source $CALOS_INSTALL/config/timezones.sh
+source $CALOS_INSTALL/config/increase-sudo-tries.sh
+source $CALOS_INSTALL/config/increase-lockout-limit.sh
+source $CALOS_INSTALL/config/ssh-flakiness.sh
+source $CALOS_INSTALL/config/detect-keyboard-layout.sh
+source $CALOS_INSTALL/config/localdb.sh
+source $CALOS_INSTALL/config/hardware/network.sh
+# source $CALOS_INSTALL/config/hardware/fix-fkeys.sh
+source $CALOS_INSTALL/config/hardware/bluetooth.sh
+source $CALOS_INSTALL/config/hardware/usb-autosuspend.sh
+source $CALOS_INSTALL/config/hardware/ignore-power-button.sh
+source $CALOS_INSTALL/config/hardware/nvidia.sh
 
 # Login
-source $OMARCHY_INSTALL/login/limine.sh
+source $CALOS_INSTALL/login/limine.sh
 
 sudo cp ~/.local/share/calos/install/boot.jpg /boot/boot.jpg
 sudo cp ~/.local/share/calos/install/bash_profile ~/.bash_profile
