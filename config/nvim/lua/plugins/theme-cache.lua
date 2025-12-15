cd13d0cfe033503672fabd8d0bb11ec392d4c883
criticalart <heels.rower.7z@icloud.com>
2025-12-14 23:40:28 -0800

    Create theme-cache.lua

---
+++ config/nvim/lua/plugins/theme-cache.lua
@@ -0,0 +1,54 @@
+return {
+  {
+    "ribru17/bamboo.nvim",
+    lazy = true,
+    priority = 1000,
+  },
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    lazy = true,
+    priority = 1000,
+  },
+  {
+    "vague-theme/vague",
+    name = "vague",
+    lazy = true,
+    priority = 1000,
+  },
+  {
+    "sainnhe/everforest",
+    lazy = true,
+    priority = 1000,
+  },
+  {
+    "ellisonleao/gruvbox.nvim",
+    lazy = true,
+    priority = 1000,
+  },
+  {
+    "rebelot/kanagawa.nvim",
+    lazy = true,
+    priority = 1000,
+  },
+  {
+    "tahayvr/matteblack.nvim",
+    lazy = true,
+    priority = 1000,
+  },
+  {
+    "loctvl842/monokai-pro.nvim",
+    lazy = true,
+    priority = 1000,
+  },
+  {
+    "shaunsingh/nord.nvim",
+    lazy = true,
+    priority = 1000,
+  },
+  {
+    "folke/tokyonight.nvim",
+    lazy = true,
+    priority = 1000,
+  },
+}
