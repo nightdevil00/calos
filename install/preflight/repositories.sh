0bad793a0be11f5ef4b4271de9c6abfefd8c7826
criticalart <heels.rower.7z@icloud.com>
2025-12-16 11:47:19 -0800

    Update repositories.sh

--- install/preflight/repositories.sh
+++ install/preflight/repositories.sh
@@ -1,16 +1,9 @@
 #!/bin/bash
 
-# Install build tools
-sudo pacman -S --needed --noconfirm base-devel
-
 # Add fun and color and verbosity to the pacman installer
 if ! grep -q "ILoveCandy" /etc/pacman.conf; then
   sudo sed -i '/^\[options\]/a Color\nILoveCandy\nVerbosePkgLists' /etc/pacman.conf
 fi
-
-# Set mirrors to global ones only
-# echo -e "Server = https://geo.mirror.pkgbuild.com/\$repo/os/\$arch\nServer = https://mirror.rackspace.com/archlinux/\$repo/os/\$arch" |
-#  sudo tee /etc/pacman.d/mirrorlist >/dev/null
 
 # Add Chaotic AUR to make the install less cancer
 
@@ -19,7 +12,11 @@ sudo pacman-key --lsign-key 3056513887B78AEB
 sudo pacman -U --noconfirm 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst'
 sudo pacman -U --noconfirm 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
 
-echo -e '\n[chaotic-aur]\nInclude = /etc/pacman.d/chaotic-mirrorlist' | sudo tee -a /etc/pacman.conf >/dev/null
+if ! grep -q "chaotic-aur" /etc/pacman.conf; then
+  echo -e '\n[chaotic-aur]\nInclude = /etc/pacman.d/chaotic-mirrorlist' | sudo tee -a /etc/pacman.conf >/dev/null
+fi
+else
+  echo -e "Chaotic-AUR already found in pacman.conf. Resuming install..."
 
 # Refresh all repos
 sudo pacman -Syu --noconfirm
