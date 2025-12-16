2b27b725c3db7357748c5e97b80661b31fa2bbe5
criticalart <heels.rower.7z@icloud.com>
2025-12-16 12:03:13 -0800

    Update misc.sh

--- install/misc.sh
+++ install/misc.sh
@@ -10,3 +10,22 @@ sudo sed -i 's|^\(auth\s\+\[default=die\]\s\+pam_faillock.so\)\s\+authfail.*$|\1
 # Give the user 10 instead of 3 tries to fat finger their password before lockout
 echo "Defaults passwd_tries=10" | sudo tee /etc/sudoers.d/passwd-tries
 sudo chmod 440 /etc/sudoers.d/passwd-tries
+
+# Disable USB autosuspend to prevent peripheral disconnection issues
+if [[ ! -f /etc/modprobe.d/disable-usb-autosuspend.conf ]]; then
+  echo "options usbcore autosuspend=-1" | sudo tee /etc/modprobe.d/disable-usb-autosuspend.conf
+fi
+
+# Install iwd explicitly if it wasn't included in archinstall
+# This can happen if archinstall used ethernet
+if ! command -v iwctl &>/dev/null; then
+  sudo pacman -S --noconfirm --needed iwd
+  chrootable_systemctl_enable iwd.service
+fi
+
+# Prevent systemd-networkd-wait-online timeout on boot
+sudo systemctl disable systemd-networkd-wait-online.service
+sudo systemctl mask systemd-networkd-wait-online.service 
+
+# Turn on bluetooth by default
+sudo systemctl enable bluetooth.service
