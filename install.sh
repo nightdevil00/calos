96dd2241ed3962f1f5efdf1aecf6d6d2c0fb4422
criticalart <heels.rower.7z@icloud.com>
2025-12-16 14:34:30 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@ sleep 1
 echo "Welcome to calOS! The installer will begin by enabling the Chaotic-AUR to help make the install faster and more reliable."
 sleep 1
 echo "paru will be installed by default. If you would like to remove Chaotic-AUR after the installation, proceed to /etc/pacman.conf"
-echo "and remove the final two lines. Don't forget to uncomment multi-lib for Steam support as well!"
+echo "and remove the final two lines. The installer will also enable the multilib repositories by default. This helps with NVIDIA detection."
 sleep 3
 echo
 echo "Installation starting..."
@@ -20,25 +20,19 @@ source $CALOS_INSTALL/preflight/repositories.sh
 sudo pacman -S --needed base-devel
 sudo pacman -S --noconfirm --needed paru
 clear
-echo "Chaotic-AUR and paru have been installed. Now installing core utils."
-sleep 2
+echo "Chaotic-AUR and paru have been installed."
+sleep 1
+echo "The installer will now begin installing AUR programs required for system functionality."
+sleep 3
 sudo pacman -S --noconfirm --needed yaru-icon-theme clipse
-#git clone https://aur.archlinux.org/paru.git
-#cd paru
-#makepkg -si --noconfirm
-
-#echo "paru installed! Resuming install..."
-#sleep 5
-#clear
-#cd ~/.local/share/calos
 
 paru -S --noconfirm --needed python-terminaltexteffects rose-pine-hyprcursor gpu-screen-recorder elephant elephant-desktopapplications elephant-menus elephant-calc walker --skipreview --removemake --cleanafter
 # yay -S --noconfirm --needed python-terminaltexteffects rose-pine-hyprcursor gpu-screen-recorder elephant elephant-desktopapplications elephant-menus elephant-calc walker --removemake --cleanafter
 
 clear
-echo "All utils successfully installed."
-sleep 1
-echo "Main packages and configuration files will now be installed..."
+echo "AUR system packages built and installed successfully! Build files will be cleaned post-installation."
+sleep 2
+echo "Main packages and configuration files next. Resuming install..."
 sleep 3
 
 source $CALOS_INSTALL/packages.sh
@@ -54,35 +48,42 @@ echo "Checking hardware for Nvidia GPU..."
 sleep 1
 echo "Installer will download/update all required configuration files if found."
 sleep 1
+echo "If an AMD gpu is detected the installation script will instead enable rocm support for system integration."
+sleep 4
 
 source $CALOS_INSTALL/nvidia.sh
 sleep 2
-clear 
-echo "Resuming Installation..."
+clear
+echo "Limine bootloader configuration will now be installed."
 sleep 1
+echo "By default, snapper services are not configured but calOS allows for full Snapshot integration with the bootloader."
+sleep 1
+echo "Refer to limine-snapper-sync in order to complete this setup yourself."
+sleep 1
+echo "Resuming Installation..."
+sleep 5
 source $CALOS_INSTALL/limine.sh
 
 sudo cp ~/.local/share/calos/install/boot.jpg /boot/boot.jpg
 sudo cp ~/.local/share/calos/install/bash_profile ~/.bash_profile
 sleep 2
 clear
-echo "Creating login service."
-sleep 2
+echo "Creating login service..."
+sleep 5
 sudo cp ~/.local/share/calos/install/greet-config.toml /etc/greetd/config.toml
 sudo cp ~/.local/share/calos/install/motd /etc/motd
-# paru -S --noconfirm --needed rose-pine-hyprcursor --skipreview --removemake --cleanafter
 echo "$USER ALL=(ALL:ALL) NOPASSWD: /usr/bin/systemctl start bootmsg.service" | sudo tee "/etc/sudoers.d/no-bootmsg-prompt"
 sudo cp ~/.local/share/calos/install/bootmsg.service /etc/systemd/system/bootmsg.service
 echo
-echo "Boot message service added and enabled."
-sleep 3
-xdg-settings set default-web-browser firefox.desktop
+echo
+echo
+echo "Boot message service added and enabled. This is located in your home directory's .bashrc file. Configure it as needed."
+sleep 5
 
-echo
-echo
 clear
 echo "Enabling polkit service and applying miscellaneous fixes..."
-sleep 2
+sleep 3
+xdg-settings set default-web-browser firefox.desktop
 echo
 systemctl --user enable --now hyprpolkitagent.service
 sudo chmod 666 /dev/uinput
@@ -97,8 +98,14 @@ systemctl --user start elephant.service
 systemctl --user enable waybar.service
 echo
 echo
-sleep 2
+clear
+
+echo "The installer will now begin removing unncessary files from this directory."
+sleep 1
+echo "Please keep all remaining files within this directory for system stability."
+sleep 1
 echo "Cleaning up installation..."
+sleep 5
 sudo rm -rf ~/go/
 #rm -rf ~/.local/share/calos/paru/
 chmod +x ~/.local/share/calos/bin/calos-pkg-list
@@ -106,12 +113,15 @@ rm ~/.local/share/calos/bin/calos-tui-install
 cp ~/.local/share/calos/applications/hidden/* ~/.local/share/applications/
 cp ~/.local/share/calos/applications/nvim.desktop ~/.local/share/applications/
 cp ~/.local/share/calos/applications/limine-snapper-restore.desktop ~/.local/share/applications/
+rm -rf ~/.local/share/calos/applications
+rm -rf ~/.local/share/calos/install
+rm -rf ~/.local/share/calos/config
+
 sudo pacman -Rns maven --noconfirm
 #sudo pacman -Rns rust --noconfirm
 sudo updatedb
 clear
 cat ~/.local/share/calos/logo.txt
-# Reboot
 echo
 echo
 echo "Installation completed. Reboot to access system."
