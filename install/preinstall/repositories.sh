8342203311855b32c9979c135232ce2d7aa05256
criticalart <heels.rower.7z@icloud.com>
2025-12-17 20:44:54 -0800

    Update repositories.sh

--- install/preinstall/repositories.sh
+++ install/preinstall/repositories.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-# Add fun and color and verbosity to the pacman installer
+# Got to have the PACMANs (copy right bandai namco 1923)
 if ! grep -q "ILoveCandy" /etc/pacman.conf; then
   sudo sed -i '/^\[options\]/a Color\nILoveCandy\nVerbosePkgLists' /etc/pacman.conf
 fi
 
-# Add Chaotic AUR to make the install less cancer
+# Add Chaotic AUR to make the install less cancer with less shit to build
 
 sudo pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
 sudo pacman-key --lsign-key 3056513887B78AEB
@@ -18,7 +18,7 @@ if ! grep -q "chaotic-aur" /etc/pacman.conf; then
   echo -e "Chaotic-AUR already found in pacman.conf. Resuming install..."
 fi
 
-# Enable multilib for Steam and Nvidia fuckery
+# Enable multilib for Steam and Nvidia fuckery, steam install will take place later on 
 
 sudo sed -i "/\[multilib\]/,/Include/"'s/^#//' /etc/pacman.conf
 
