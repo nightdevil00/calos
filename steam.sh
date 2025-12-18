b67fa7082975dd4fd6dbee2c8b42409d701d70e7
criticalart <heels.rower.7z@icloud.com>
2025-12-17 21:01:03 -0800

    Update steam.sh

--- steam.sh
+++ steam.sh
@@ -4,9 +4,9 @@ while true; do
 
 read -p "Would you like to install Steam (y/n) " yn
 case $yn in
-  [yY] ) echo "Installing Steam...";
+  [yY] ) echo "Initializing Steam installer...";
     break;;
-  [nN] ) echo "Steam will not be installed. Please reboot system.";
+  [nN] ) echo "Steam will not be installed. Please reboot system. To access bios directly, type: systemctl reboot --firmware-setup in the terminal.";
     exit;;
   * ) echo "Invalid response. Please specify (y/n)";;
 esac
@@ -14,12 +14,16 @@ esac
 done
 sleep 1
 clear
-echo "Steam will now be installed. If prompted to select your driver, make sure to specify vulkan-radeon."
-sleep 3
+echo "Steam will now be installed. If prompted to select your drivers reccomendations are as follows:"
+sleep 1
+echo "For AMD GPUs, please use vulkan-radeon."
+sleep 1
+echo "NVIDIA GPUs should have proper drivers installed already from the earlier script. If not, use nvidia-utils."
+sleep 5
 echo "Installing..."
 sudo pacman --needed -S steam
 echo
 sed -i "/exec-once/"'s/^#//' ~/.config/hypr/autostart.conf
-echo "Steam installed. System will reboot in 5 seconds..."
+echo "Steam installed. System will reboot to BIOS in 5 seconds..."
 sleep 5
-systemctl reboot
+systemctl reboot --firmware-setup
