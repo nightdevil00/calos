5f258fee917b0c82c1e9687dfc7847898ad92032
criticalart <heels.rower.7z@icloud.com>
2025-12-20 16:59:51 -0800

    Update branding.sh

--- install/config/branding.sh
+++ install/config/branding.sh
@@ -2,6 +2,6 @@
 
 # Allow the user to change the branding for fastfetch and screensaver
 mkdir -p ~/.config/calos/branding
-cp ~/.local/share/calos/icon.txt ~/.config/calos/branding/about.txt
-cp ~/.local/share/calos/logo.txt ~/.config/calos/branding/screensaver.txt
-cp ~/.local/share/calos/blank.txt ~/.config/calos/branding/blank.txt
+cp ~/.local/share/calos/install/icon.txt ~/.config/calos/branding/about.txt
+cp ~/.local/share/calos/install/logo.txt ~/.config/calos/branding/screensaver.txt
+cp ~/.local/share/calos/install/blank.txt ~/.config/calos/branding/blank.txt
