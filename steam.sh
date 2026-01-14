d3dc60531f967b5dcafc6b63e2aa06aec269b3db
criticalart <heels.rower.7z@icloud.com>
2026-01-14 05:37:08 -0800

    Enhance Steam installation instructions in steam.sh
    Updated installation instructions for Steam to include repository options and improved formatting for GPU driver guidance.

--- steam.sh
+++ steam.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-gum style --border normal --border-foreground 212 --padding="1 3" "Steam will now be installed. If prompted to select your drivers choose the following." "For AMD GPUs, please use vulkan-radeon. For the second prompt, if asked, use lib32-vulkan-radeon." "NVIDIA GPUs should already have total proper driver support from this installer."
+gum style --border normal --border-foreground 212 --padding="1 3" "Steam will now be installed. If prompted to select your repository/drivers choose the following:" "You may use either the $(gum style --italic 'extra') repository or $(gum style --italic 'chaotic-aur')." " " "For AMD GPUs, please use $(gum style --bold --foreground 212 'vulkan-radeon'). For the second prompt, if asked, use $(gum style --bold --foreground 212 'lib32-vulkan-radeon')." "NVIDIA GPUs should already have total proper driver support from this installer."
 echo
 sleep 8
 gum spin -s line --title="Installing Steam..." -- sleep 2
