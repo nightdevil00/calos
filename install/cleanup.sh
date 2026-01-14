53755f604b5172e18beddb859cf1aeb2c0532c33
criticalart <heels.rower.7z@icloud.com>
2026-01-14 15:22:51 -0800

    Add cleanup script for Calos installation
    This script cleans up the Calos installation by removing unnecessary files and directories, updating the database, and displaying a logo.

---
+++ install/cleanup.sh
@@ -0,0 +1,14 @@
+chmod +x ~/.local/share/calos/bin/calos-pkg-list
+chmod +x ~/.local/share/calos/bin/calos-list-keybindings
+rm ~/.local/share/calos/bin/calos-tui-install
+cp ~/.local/share/calos/applications/hidden/* ~/.local/share/applications/
+cp ~/.local/share/calos/applications/nvim.desktop ~/.local/share/applications/
+rm -rf ~/.local/share/calos/applications
+rm -rf ~/.local/share/calos/config
+rm -rf ~/.local/share/calos/.git
+sudo updatedb
+clear
+cat ~/.local/share/calos/install/logo-complete.txt | tte --xterm-colors --frame-rate 60 middleout
+echo
+rm -rf ~/.local/share/calos/install
+rm ~/.local/share/calos/README.md
