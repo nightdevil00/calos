d2bd6d61d0824b27f0a428daa01ec58c48a554d4
criticalart <heels.rower.7z@icloud.com>
2026-01-14 01:52:42 -0800

    Update installation messages in steam.sh

--- steam.sh
+++ steam.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-gum style --border normal --border-foreground 212 --padding="1 3" "Steam will now be installed. If prompted to select your drivers reccomendations are as follows." "For AMD GPUs, please use vulkan-radeon. For the second prompt, if asked, use lib32-vulkan-radeon." "NVIDIA GPUs should already have proper driver support from this installer."
+gum style --border normal --border-foreground 212 --padding="1 3" "Steam will now be installed. If prompted to select your drivers choose the following." "For AMD GPUs, please use vulkan-radeon. For the second prompt, if asked, use lib32-vulkan-radeon." "NVIDIA GPUs should already have total proper driver support from this installer."
 echo
 sleep 8
-gum spin -s line --title="Install Steam..." -- sleep 2
+gum spin -s line --title="Installing Steam..." -- sleep 2
 sudo pacman --needed -S steam
 echo
 sed -i "/exec-once/"'s/^#//' ~/.config/hypr/autostart.conf
