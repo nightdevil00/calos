616d713f487566f6d4c30e4a27b49eb34dcc7c49
criticalart <heels.rower.7z@icloud.com>
2026-01-14 01:51:40 -0800

    Refactor installer messages for clarity and consistency

--- install.sh
+++ install.sh
@@ -9,10 +9,11 @@ CALOS_INSTALL=~/.local/share/calos/install
 sudo pacman -S --noconfirm --needed gum
 
 clear
-gum style --border normal --border-foreground 212 --padding="1 3" "Welcome to $(gum style --bold --foreground 212 'calOS')!" "This script will turn your base Arch Linux install into a minimal, yet functional, Hyprland setup." "The installer will install/enable Chaotic-AUR as well as paru to function as your AUR helper." "Make sure you are running this installation script on a $(gum style --foreground 212 'fresh Arch Linux installation')!"
+gum style --border normal --border-foreground 212 --padding="1 3" "Welcome to $(gum style --bold --foreground 212 'calOS')!" " " "This script will turn your base Arch Linux install into a minimal, yet functional, Hyprland setup." "The installer will install/enable Chaotic-AUR as well as paru to function as your AUR helper." " " "Make sure you are running this installation script on a $(gum style --foreground 212 'fresh Arch Linux installation')!"
 echo
 sleep 6
 gum confirm "Proceed with Install?" && gum spin -s line --title="Installation starting..." -- sleep 2 || exit 1
+echo
 
 # Scripts for error reporting and repository edits
 
@@ -24,7 +25,7 @@ clear
 
 # System critical AUR packages
 
-gum style --border normal --border-foreground 212 --padding="1 3" "Chaotic-AUR repository synced and paru has been installed." "The installer will now begin downloading and building AUR packages required for basic system functionality." "This is the most time-consuming portion of the install. Time spent will vary based on your hardware."
+gum style --border normal --border-foreground 212 --padding="1 3" "Chaotic-AUR repository synced and paru has been installed." " " "The installer will now begin downloading and building AUR packages required for basic system functionality." "This is the most time-consuming portion of the install. Time spent will vary based on your hardware."
 sleep 5
 echo
 gum spin -s line --title="Resuming install..." -- sleep 4
@@ -34,7 +35,7 @@ clear
 
 # Main packages, configuration and scripts
 
-gum style --border normal --border-foreground 212 --padding="1 3" "AUR system packages built and installed successfully! Build files will be cleansed post-installation." "The installer will now download/install all required packages and link required configuration files."
+gum style --border normal --border-foreground 212 --padding="1 3" "AUR system packages built and installed successfully! Build files will be cleansed post-installation." " " "The installer will now download/install all required packages and link required configuration files."
 echo
 sleep 4
 gum spin -s line --title="Resuming install..." -- sleep 4
@@ -50,7 +51,7 @@ clear
 
 # NVIDIA checks, AMD fixes
 
-gum style --border normal --border-foreground 212 --padding="1 3" "The installer will now prompt you to select your GPU architecture." "If an AMD is selected the installer will install and enable rocm support for complete sytem monitor integration." "Choosing NVIDIA will run the NVIDIA installation script, which will then scan your hardware and download required dependencies/headers."
+gum style --border normal --border-foreground 212 --padding="1 3" "The installer will now prompt you to select your GPU architecture." "If AMD is selected the installer will download and enable rocm support for complete sytem monitor integration." "Choosing NVIDIA will download required dependencies/headers as well as update hyprland configurations."
 echo
 sleep 2
 GPU=$(gum choose --item.foreground 250 "AMD" "NVIDIA")
@@ -59,7 +60,7 @@ clear
 
 # Limine bootloader setup
 
-gum style --border normal --border-foreground 212 --padding="1 3" "The installer will now check which bootloader you have installed." "The default recommended bootloader is Limine, however, other bootloaders are fully supported." "If Limine is detected the installer will enable various features, such as automated mkinitcpio/dual-booting."
+gum style --border normal --border-foreground 212 --padding="1 3" "The installer will now check which bootloader you have installed." "The default recommended bootloader is Limine, but calOS will function on any." " " "If Limine is detected the installer will enable various features, such as automated mkinitcpio/dual-booting."
 sleep 6
 echo
 gum spin -s line --title="Resuming install..." -- sleep 4
@@ -92,11 +93,12 @@ clear
 
 # AUR helper
 
-gum style --border normal --border-foreground 212 --padding="1 3" "Please specify which AUR helper you would like to utilize on your system." "By default the installer ships with and utilizes Paru." "Paru is relatively faster than yay with built-in PKGBUILD viewing in terminal." "If you would still like to switch back to yay, please specify below."
+gum style --border normal --border-foreground 212 --padding="1 3" "Please specify which AUR helper you would like to utilize on your system." " " "By default the installer ships with and utilizes Paru." "Paru is relatively faster than yay with built-in PKGBUILD viewing in terminal." " " "If you would like to switch back to yay, please specify below."
 echo
 sleep 4
 HELPER=$(gum choose --item.foreground 250 "Paru" "yay")
-[[ "$HELPER" == "yay" ]] && gum spin -s line --title="paru will now be replaced with yay..." -- sleep 3 && sudo pacman -S --noconfirm --needed yay && sudo pacman -Rns paru --noconfirm || gum spin -s line --title="paru will remain as your AUR helper. Resuming install..." -- sleep 4
+[[ "$HELPER" == "yay" ]] && gum spin -s line --title="paru will now be replaced with yay..." -- sleep 3 && sudo pacman -S --noconfirm --needed yay && sudo pacman -Rns paru --noconfirm || gum spin -s line --title="Paru will remain your AUR helper. Resuming install..." -- sleep 4
+clear
 
 # Installation Cleanup
 
@@ -120,12 +122,11 @@ rm ~/.local/share/calos/README.md
 
 # Installation Completion and Optional Steam Install
 
-gum style --border normal --border-foreground 212 --padding="1 3" "$(gum style --bold --foreground 212 'Installation complete'), reboot to access system. Your username will be remembered by the greeter after your first login." "Make sure to read through your configuration files to familarize yourself with system operations., especially those in $(gum style --italic '~/.config/hypr')." "It is heavily recommended to configure your $(gum style --italic '~/.config/hypr/monitors.conf') file in order to set a proper resolution and refresh rate." "Lastly, if you would like to install Steam, please follow the prompts below. Multilib repositories have already been enabled."
+gum style --border normal --border-foreground 212 --padding="1 3" "$(gum style --bold --foreground 212 'Installation complete'), reboot to access system." " " "Make sure to read through your configuration files to familarize yourself with the OS, especially in $(gum style --italic '~/.config/hypr')." "Please configure your $(gum style --italic '~/.config/hypr/monitors.conf') file in order to set a proper resolution and refresh rate." " " "Lastly, if you would like to install Steam, follow the prompts below. Multilib repositories have already been enabled."
 sleep 8
 echo
 echo
 gum confirm "Install Steam?" && gum spin -s line --title="Initializing Steam installation script..." -- sleep 4 && source ./steam.sh || echo "Steam will not be installed."
 echo
 echo
-sleep 2
 gum confirm "Would you like to reboot to BIOS?" && systemctl reboot --firmware-setup
