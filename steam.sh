b7cf8f37834468223e69b9b08dc54300c5096732
criticalart <heels.rower.7z@icloud.com>
2026-01-18 19:06:34 -0800

    Update steam.sh

--- steam.sh
+++ steam.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+clear
 gum style --border normal --border-foreground 212 --padding="1 3" "Steam will now be installed. If prompted to select your repository/drivers choose the following:" "Only use the $(gum style --italic 'extra/multilib') repository. Do not use $(gum style --italic 'Chaotic-aur')." " " "For AMD GPUs, please use $(gum style --bold --foreground 212 'vulkan-radeon'). For the second prompt, if asked, use $(gum style --bold --foreground 212 'lib32-vulkan-radeon')." "NVIDIA GPUs should already have total proper driver support from this installer."
 echo
 sleep 8
